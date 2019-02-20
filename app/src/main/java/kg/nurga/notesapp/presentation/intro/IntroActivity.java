62c0a3d315f50053073a60e0041ee89c2fa0504a
Nurgazy <n.sulaimanov12@gmail.com>
2019-02-20 15:33:46 +0600

    Home work,added skip button and sharedPreferencees

--- app/src/main/java/kg/nurga/notesapp/presentation/intro/IntroActivity.java
+++ app/src/main/java/kg/nurga/notesapp/presentation/intro/IntroActivity.java
@@ -1,6 +1,9 @@
 package kg.nurga.notesapp.presentation.intro;
 
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.SharedMemory;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -14,6 +17,7 @@ import android.widget.TextView;
 import com.badoualy.stepperindicator.StepperIndicator;
 
 import kg.nurga.notesapp.R;
+import kg.nurga.notesapp.presentation.launch.LaunchActivity;
 import kg.nurga.notesapp.presentation.main.MainActivity;
 
 
@@ -21,8 +25,10 @@ public class IntroActivity extends AppCompatActivity implements View.OnClickList
     private IntroPagerAdapter mIntroAdapter;
     private ViewPager mViewPager;
 
-    private TextView nextBtn;
+    private TextView nextBtn, skipBtn;
     private StepperIndicator mStepperIndicator;
+
+    private SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,11 @@ public class IntroActivity extends AppCompatActivity implements View.OnClickList
         initViewPager();
         nextBtn = findViewById(R.id.intro_next_btn);
         nextBtn.setOnClickListener(this);
+        skipBtn = findViewById(R.id.intro_skip_btn);
+        skipBtn.setOnClickListener(this);
+
+        mSharedPreferences = getSharedPreferences("settings", MODE_PRIVATE);
+
     }
 
     private void initViewPager(){
@@ -86,7 +97,17 @@ public class IntroActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId()) {
             case R.id.intro_next_btn:
                 onNextClick();
+                mSharedPreferences.edit().putBoolean("tutorial",true).apply();
+                Intent intent = new Intent(v.getContext(), LaunchActivity.class);
+                v.getContext().startActivity(intent);
+                finish();
                 break;
+            case R.id.intro_skip_btn:
+                MainActivity.start(this);
+                mSharedPreferences.edit().putBoolean("tutorial",true).apply();
+                Intent intent1 = new Intent(v.getContext(), LaunchActivity.class);
+                v.getContext().startActivity(intent1);
+                finish();
         }
     }
 
--- app/src/main/java/kg/nurga/notesapp/presentation/launch/LaunchActivity.java
+++ app/src/main/java/kg/nurga/notesapp/presentation/launch/LaunchActivity.java
@@ -1,16 +1,31 @@
 package kg.nurga.notesapp.presentation.launch;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import kg.nurga.notesapp.R;
+import kg.nurga.notesapp.presentation.intro.IntroActivity;
 
 
 public class LaunchActivity extends AppCompatActivity {
+    SharedPreferences mSharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launch);
+        mSharedPreferences = getSharedPreferences("settings", MODE_PRIVATE);
+        boolean tutorialWasShown = mSharedPreferences.getBoolean("tutorial", false);
+        if (!tutorialWasShown) {
+
+            startActivity(new Intent(this, IntroActivity.class));
+            finish();
+
+        }else {
+            setContentView(R.layout.activity_launch);
+
+        }
     }
 }
